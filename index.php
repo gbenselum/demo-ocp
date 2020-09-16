56ab41454f4ef861e9a565e7dcb1bc7979934716
Gabrielzeven <49731558+gbenselum@users.noreply.github.com>
2020-09-16 18:01:05 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<html>
+<head>
+<title > Probando sitio en php</title>
+<title > Version 1.1 </title>
+
+</head>
+<body>
+<?php
+ echo "Hola a todos desde <b>";
+ echo $_SERVER['SERVER_ADDR'];
+ echo "<br><br>nueva liena desde webhook</b>";
+ echo "<br><br>prueba 2";
+ echo "<br><br><b>HOLA GABO</b> ";
+ echo "</body>";
+
+
+?>
