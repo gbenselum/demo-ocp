398afbe17b0a924324ab239026c1ca2a3ec373c6
Gabrielzeven <49731558+gbenselum@users.noreply.github.com>
2020-09-16 18:22:07 -0300

    Update server.py

--- server.py
+++ server.py
@@ -12,14 +12,3 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 
 httpd = socketserver.TCPServer(('', 8000), Handler)
 httpd.serve_forever()
-
-
-# python3 server.py
-# 127.0.0.1 - - [11/Apr/2017 11:36:49] "GET / HTTP/1.1" 200 -
-# http :8000
-'''
-HTTP/1.0 200 OK
-Date: Tue, 11 Apr 2017 15:36:49 GMT
-Server: SimpleHTTP/0.6 Python/3.5.2
-Hello world
-'''
